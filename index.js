a61a4db620bcd12640059516357d7d9e0cb2e20a
Supphitan Paksawad <Supphitan.p@kkumail.com>
2022-09-27 10:26:04 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mysql = require("mysql");
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+app.get("/", function(req, res) {
+    return res.send({ error: true, message: "Test Employee Web API" });
+});
+
+var dbConn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "Kariscore",
+});
+
+dbConn.connect();
+
+app.get("/allsubject", function(req, res) {
+    dbConn.query("SELECT * FROM Subject", function(error, results, fields) {
+        if (error) throw error;
+        return res.send(results);
+    });
+});
+
+
+//set port
+app.listen(3000, function() {
+    console.log("Node app is running on port 3000");
+});
+
+module.exports = app;
