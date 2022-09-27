727465e12cefefe72636c572cd5ea84917807e37
Supphitan Paksawad <Supphitan.p@kkumail.com>
2022-09-27 10:37:30 +0700

    update

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("/allsubject", function(req, res) {
 
 
 //set port
-app.listen(3000, function() {
+app.listen(4000, function() {
     console.log("Node app is running on port 3000");
 });
 
