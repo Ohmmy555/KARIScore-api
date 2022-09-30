e4efb18506a39f186dce85725d5f763b164aa189
Supphitan Paksawad <Supphitan.p@kkumail.com>
2022-09-30 17:38:24 +0700

    update insert

--- index.js
+++ index.js
@@ -48,6 +48,26 @@ app.put("/login/:email", function(req, res) {
         });
 })
 
+app.put('/signup/student', function(req, res) {
+    let data = req.body;
+    let stdid = data['std_id'];
+    let stdname = data['std_name'];
+    let stdemail = data['std_email'];
+    let stdpassword = dara['std_password'];
+    let type = 1
+    if (!stdid) {
+        return res.status(400).send({ error: true, message: 'Please provide student id and student data' });
+    }
+
+    dbConn.query(
+        "INSERT INTO Users(student_id, user_name, user_email, user_pass, idUser_type) VALUES ? ", [stdid, stdname, stdemail, stdpassword, type],
+        function(error, results, fields) {
+            if (error) throw error;
+            return res.send(results);
+        }
+    );
+})
+
 //set port
 app.listen(4000, function() {
     console.log("Node app is running on port 3000");
